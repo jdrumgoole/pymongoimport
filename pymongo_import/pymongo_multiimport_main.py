c62c6b587dce4833ca7d7e97caedac2da5833daa
Joe Drumgoole <joe.drumgoole@mongodb.com>
2018-04-22 23:41:33 +0100

    Fixed typo in setup.py made fixes to command line arg processing

--- pymongo_import/pymongo_multiimport_main.py
+++ pymongo_import/pymongo_multiimport_main.py
@@ -9,15 +9,15 @@ import time
 import copy
 import os
 import pymongo
-
+import glob
 
 from pymongo_import.filesplitter import File_Splitter
-from pymongo_import.pymdb_import import mongo_import
+from pymongo_import.pymongo_import_main import mongo_import
 from pymongo_import.logger import Logger
 from pymongo_import.argparser import add_standard_args
 
-  
-def strip_arg( arg_list, remove_arg, has_trailing=False ):
+
+def strip_arg(arg_list, remove_arg, has_trailing=False):
     '''
     Remove arg and arg argument from a list of args. If has_trailing is true then
     remove --arg value else just remove --arg.
@@ -29,27 +29,28 @@ def strip_arg( arg_list, remove_arg, has_trailing=False ):
     has_trailing (boolean) : If the arg in `remove_arg` has an arg. Then make sure
     to remove that arg as well
     '''
-    try :
-        location = arg_list.index( remove_arg )
+    try:
+        location = arg_list.index(remove_arg)
         if has_trailing:
-            del arg_list[ location + 1 ]
-        del arg_list[ location]
-        
-    except ValueError :
-        pass
-    
-    return arg_list       
+            del arg_list[location + 1]
+        del arg_list[location]
 
-def multi_import( *argv ):
+    except ValueError:
+        pass
+
+    return arg_list
+
+
+def multi_import(*argv):
     """
-.. function:: mutlti_import ( *argv )
+.. function:: multi_import ( *argv )
 
    Import CSV files using multiprocessing
 
    :param argv: list of command lines
 
    """
-    
+
     usage_message = '''
     
     A master script to manage uploading of a single data file as multiple input files. Multi-import
@@ -57,97 +58,101 @@ def multi_import( *argv ):
     already split list of files passed in on the command line.
     Each file is uplaoded by a separate pymongoimport subprocess. 
     '''
-    
-    parser = argparse.ArgumentParser( usage=usage_message)
-    parser = add_standard_args( parser )
-    parser.add_argument( "--autosplit", type=int,
-                         help="split file based on loooking at the first ten lines and overall file size [default : %(default)s]")
-    parser.add_argument( "--splitsize", type=int, help="Split file into chunks of this size [default : %(default)s]" )
 
-    args= parser.parse_args( *argv )
-    
-    log = Logger( "multiimport" ).log()
-    
-    Logger.add_file_handler( "multiimport" )
-    Logger.add_stream_handler( "multiimport" )
-    
+    parser = argparse.ArgumentParser(usage=usage_message)
+    parser = add_standard_args(parser)
+    parser.add_argument("--autosplit", type=int,
+                        help="split file based on loooking at the first ten lines and overall file size [default : %(default)s]")
+    parser.add_argument("--splitsize", type=int, help="Split file into chunks of this size [default : %(default)s]")
+    parser.add_argument("--usesplits", action="store_true", default=False, help="Use the split files already created by by a previous autosplit")
+
+    args = parser.parse_args(*argv)
+
+    log = Logger("multi_import").log()
+
+    Logger.add_file_handler("multi_import")
+    Logger.add_stream_handler("mult_iimport")
+
     child_args = sys.argv[1:]
     children = OrderedDict()
-    
-    print( args.filenames )
-    if len( args.filenames ) == 0 :
-        log.info( "no input file to split")
-        sys.exit( 0 )
-    
-    if args.autosplit or args.splitsize :
-        if len( args.filenames) > 1 :
-                log.warn( "More than one input file specified ( '%s' ) only splitting the first file:'%s'", 
-                       " ".join( args.filenames ), args.filenames[ 0 ] )
+
+    print(args.filenames)
+    if len(args.filenames) == 0:
+        log.info("no input file to split")
+        sys.exit(0)
+
+    if args.autosplit or args.splitsize or args.usesplits:
+        if len(args.filenames) > 1:
+            log.warn("More than one input file specified ( '%s' ) only splitting the first file:'%s'",
+                     " ".join(args.filenames), args.filenames[0])
         if args.autosplit:
-            child_args = strip_arg( child_args, "--autosplit", True)
+            child_args = strip_arg(child_args, "--autosplit", True)
         if args.splitsize:
-            child_args = strip_arg( child_args, "--splitsize", True )
-        
+            child_args = strip_arg(child_args, "--splitsize", True)
+        if args.usesplits:
+            child_args = strip_arg(child_args, "--usesplits", False)
 
-        splitter = File_Splitter( args.filenames[ 0 ], args.hasheader )
+        splitter = File_Splitter(args.filenames[0], args.hasheader)
 
-    for i in args.filenames: # get rid of old filenames
-        child_args = strip_arg( child_args, i, False )
+    for i in args.filenames:  # get rid of old filenames
+        child_args = strip_arg(child_args, i, False)
 
-    if args.autosplit:
-        log.info( "Autosplitting file: '%s' into (approx) %i chunks", args.filenames[ 0 ], args.autosplit )
-        files = splitter.autosplit( args.autosplit )
-    elif args.splitsize > 0 :
-        log.info( "Splitting file: '%s' into %i line chunks", args.filenames[ 0 ], args.splitsize )
-        files = splitter.split_file( args.splitsize )
+    if args.usesplits:
+        files = glob.glob( "*.[123456789]*" )
+        files.sort()
+        files = [ ( i, os.path.getsize(i)) for i in files]
+        #print("usesplits:%s" % files)
+    elif args.autosplit:
+        log.info("Autosplitting file: '%s' into (approx) %i chunks", args.filenames[0], args.autosplit)
+        files = splitter.autosplit(args.autosplit)
+    elif args.splitsize > 0:
+        log.info("Splitting file: '%s' into %i line chunks", args.filenames[0], args.splitsize)
+        files = splitter.split_file(args.splitsize)
     else:
-        files=[]
-        for i in args.filenames :
-            files.append( ( i, os.path.getsize( i )))
+        files = [ ( i, os.path.getsize(i)) for i in args.filenames]
 
     if args.restart:
-        log.info( "Ignoring --drop overridden by --restart")
+        log.info("Ignoring --drop overridden by --restart")
     elif args.drop:
-        client = pymongo.MongoClient(args.host )
-        log.info( "Dropping database : %s", args.database )
-        client.drop_database(  args.database )
-        child_args = strip_arg( child_args, args.drop )
+        client = pymongo.MongoClient(args.host)
+        log.info("Dropping database : %s", args.database)
+        client.drop_database(args.database)
+        child_args = strip_arg(child_args, args.drop)
 
     start = time.time()
 
-    process_count=0
-    try :
+    process_count = 0
+    try:
         for filename in files:
             process_count = process_count + 1
             proc_name = filename[0]
             # need to turn args to Process into a tuple )
-            new_args = copy.deepcopy( child_args )
-            #new_args.extend( [ "--logname", filename[0], filename[0] ] )
+            new_args = copy.deepcopy(child_args)
+            # new_args.extend( [ "--logname", filename[0], filename[0] ] )
             new_args.extend([filename[0]])
-            proc = Process( target=mongo_import, name=proc_name, args=(new_args, ))
-            log.info( "Processing '%s'", filename[0] )
+            proc = Process(target=mongo_import, name=proc_name, args=(new_args,))
+            log.info("Processing '%s'", filename[0])
             proc.daemon = True
-            children[ proc_name ] = { "process" : proc }
-            log.info( "starting sub process: %s", proc_name )
-            children[ proc_name ][ "start" ] = time.time()
+            children[proc_name] = {"process": proc}
+            log.info("starting sub process: %s", proc_name)
+            children[proc_name]["start"] = time.time()
             proc.start()
-            
-        for i in children.keys():
-            log.info( "Waiting for process: '%s' to complete" , i )
-            children[ i ][ "process" ].join()
-            children[ i ][ "end" ] = time.time()
-            log.info( "elapsed time for process %s : %f", i, children[ i ][ "end" ] - children[ i ][ "start"])
-            
-    except KeyboardInterrupt :
-        for i in children.keys():
-            log.info( "terminating process: '%s'", i )
-            children[ i ][ "process" ].terminate()
-            
-    finish = time.time()
-    
-    log.info( "Total elapsed time:%f" % ( finish - start ))
-    
-    
-if __name__ == '__main__':
-    multi_import( sys.argv[1:])
 
+        for i in children.keys():
+            log.info("Waiting for process: '%s' to complete", i)
+            children[i]["process"].join()
+            children[i]["end"] = time.time()
+            log.info("elapsed time for process %s : %f", i, children[i]["end"] - children[i]["start"])
+
+    except KeyboardInterrupt:
+        for i in children.keys():
+            log.info("terminating process: '%s'", i)
+            children[i]["process"].terminate()
+
+    finish = time.time()
+
+    log.info("Total elapsed time:%f" % (finish - start))
+
+
+if __name__ == '__main__':
+    multi_import(sys.argv[1:])
