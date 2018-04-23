725f0335ec4ecfd7ac0daa99d7f86906a1919ec3
Joe Drumgoole <joe.drumgoole@mongodb.com>
2018-04-23 01:04:42 +0100

    Bumped version number

--- pymongo_import/version.py
+++ pymongo_import/version.py
@@ -1,10 +1,11 @@
 """
 Version 1.4.7 : 8-Apr-2018 : Now only supports python 3.6.
 
+1.4.9a2 : 23-Apr-2016 : Added a multiprocessing processing pool via --poolsize
 1.4.9a1 : 22-Apr-2017 : Fixed typo in setup.py that stopped mongo_import running
 1.4.8a9 : 19-Apr-2017 : Renamed binaries to prevent class with package name
 1.4.8a8 : 19-Apr-2017 : Added --info argument to allow insertion of a string
 during auditing.
 
 """
-__VERSION__="1.4.9a1"  # type: str
+__VERSION__="1.4.9a2"  # type: str
