98fd8d8be4f03135317ae347199ed8e9e7c92167
Joe Drumgoole <Joe.Drumgoole@mongodb.com>
2019-11-18 12:10:25 +0000

    Rename

--- pymongoimport/excelreader/extract.sh
+++ pymongoimport/excelreader/extract.sh
@@ -3,3 +3,4 @@
 python excelreader.py --excelfile emeadevit.xlsx --sheetname Q3 --collection Q3 --drop --mincol 30 --maxcol 32 --minrow 33 --maxrow 38 --host ${MDBHOST}
 python excelreader.py --excelfile emeadevit.xlsx --sheetname Q4 --collection Q4 --drop --mincol 20 --maxcol 22 --minrow 32 --maxrow 37 --host ${MDBHOST}
 python excelreader.py --excelfile emeadevit.xlsx --sheetname Q6 --collection Q6 --drop --mincol 21 --maxcol 23 --minrow 29 --maxrow 31 --host ${MDBHOST}
+python excelreader.py --excelfile Ag
